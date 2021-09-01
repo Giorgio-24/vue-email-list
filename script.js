c374d38b27488c20d98dd8a032ddeb6f5c5ecbc5
Giorgio Andrei <cipuandrei@libero.it>
2021-09-01 15:26:16 +0200

    email-succesfully-generated

--- script.js
+++ script.js
@@ -4,9 +4,32 @@ var root = new Vue(
     {
         el: '#root',
         data: {
-
+            emailList: [],
         },
         methods: {
-
+            axiosTemplate() {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        const response = res.data.response;
+                        this.emailList.push(response);
+                    })
+                    .catch((error) => {
+                        //! VA SOLO IN CASO DI ERRORI
+                        console.log(error);
+                    });
+            }
+        },
+        created() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    const response = res.data.response;
+                    this.emailList = response;
+                })
+                .catch((error) => {
+                    //! VA SOLO IN CASO DI ERRORI
+                    console.log(error);
+                });
         },
     });
